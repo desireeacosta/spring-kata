88c8eb259241cfa0fbecb53c8c294f8a3dc1d969
desireeacosta <desiree.acosta.60086@usalesiana.edu.bo>
2023-07-21 23:39:40 -0400

    Added some tests for delete and fixed findAllTest

--- src/test/java/com/ss/web/app/service/StudentServiceImplTest.java
+++ src/test/java/com/ss/web/app/service/StudentServiceImplTest.java
@@ -3,6 +3,10 @@ package com.ss.web.app.service;
 import com.ss.web.app.model.Student;
 import com.ss.web.app.repository.StudentRepo;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,9 +26,42 @@ class StudentServiceImplTest {
     assertEquals(student, studentExpected);
   }
 
-//  @Test
-//  public void findAllTest(){
-//    StudentService studentService = new StudentService();
-//    studentService
-//  }
+  @Test
+  public void findAllTest(){
+    StudentRepo repo = mock(StudentRepo.class);
+    StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+    List<Student> students = new ArrayList<>();
+    students.add(new Student(1L, "Sho", "Test"));
+    students.add(new Student(2L, "Auto", "Test"));
+    students.add(new Student(3L, "Curl", "Test"));
+    when(repo.findAll()).thenReturn(students);
+
+    List<Student> studentsExpected = studentServiceImpl.findAll();
+
+    assertNotNull(studentsExpected);
+    assertEquals(students, studentsExpected);
+  }
+
+  @Test
+  public void delete_ExistentUser_True(){
+    StudentRepo repo = mock(StudentRepo.class);
+    StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+    Student student = new Student(3L, "Curl", "Test");
+    when(repo.delete(student)).thenReturn(true);
+
+    boolean actualResult = studentServiceImpl.delete(student);
+
+    assertTrue(actualResult);
+  }
+
+  @Test
+  public void delete_NonExistentUser_False(){
+    StudentRepo repo = mock(StudentRepo.class);
+    StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+    Student newStudent = new Student(1L, "Pepe", "Pe");
+
+    boolean actualResult = studentServiceImpl.delete(newStudent);
+
+    assertFalse(actualResult);
+  }
 }
